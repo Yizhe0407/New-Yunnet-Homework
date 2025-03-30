6afcf35836ffb1ec6f3646a4e090ffb766611f24
yizhe <liaoyizhe75@gmail.com>
2025-03-30 21:33:07 +0800

    Add Tailwind CSS configuration and implement theme provider with UI components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,29 @@
-import Image from "next/image";
+import { SparklesText } from "@/components/magicui/sparkles-text";
+import Link from "next/link";
+import { ModeToggle } from "@/components/ModeToggle";
+import { DotPattern } from "@/components/magicui/dot-pattern";
+import LoginButton from "@/components/LoginButton";
+
+
+
+
+const texts = [
+  "Yunnet",
+  "Homework"
+];
 
 export default function Home() {
+
   return (
-    <div>
-      
-    </div>
+    <main className="relative flex min-h-screen flex-col items-center justify-center gap-8 overflow-hidden">
+      <DotPattern className="absolute inset-0 opacity-50"></DotPattern>
+      <SparklesText text="Yunnet Homework" />
+
+      <LoginButton />
+
+      <ModeToggle />
+
+    </main>
+
   );
 }
