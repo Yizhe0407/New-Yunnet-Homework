868145e483daf6d580e024122690bf5802e24a2a
yizhe <liaoyizhe75@gmail.com>
2025-03-31 14:30:11 +0800

    💄 Add ts-node

--- backend/src/data/users.ts
+++ backend/src/data/users.ts
@@ -3,5 +3,5 @@ import { User } from '../models/user';
 
 export let users: User[] = [
   { id: 1, name: 'Alice', email: 'alice@example.com', createdAt: new Date() },
-  { id: 2, name: 'Bob', email: 'bob@example.com', createdAt: new Date() },
+  { id: 2, name: 'Bobji', email: 'bob@example.com', createdAt: new Date() },
 ];
